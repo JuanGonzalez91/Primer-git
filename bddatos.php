d1fecdbd931f7c863e364e07ad061c48b948469d
El cazador feroz <cazador@gmail.com>
2021-06-13 15:40:35 -0500

    Se agragó una esctructura de HTML

--- bddatos.php
+++ bddatos.php
@@ -1,3 +1,21 @@
 dwdwdw
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum?
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptatem odit quae vel atque et!
+Lorem, ipsum dolor.
+Lorem ipsum dolor sit amet.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, minus!
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>titulo</h1>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi tempora repellat magnam corporis sint,
+     totam impedit vero excepturi. Laborum, eaque!</h2>
+</body>
+</html>
